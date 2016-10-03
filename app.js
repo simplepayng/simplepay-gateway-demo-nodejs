553612fb315a693fecc4fb395cb44dba1b74697e
Pedro Gomes <pedrorodriguesgomes@gmail.com>
2016-10-03 11:23:03 +0100

    Update to gateway version 2.

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ var superAgent = require('superagent');
 var app = express();
 
 // support json encoded bodies
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var privateKey = 'test_pr_demo';
 
 app.post('/verify', function (request, result) {
     console.log('Transaction ID was: ', request.body.transaction_id); // we don't really need this here, is just an example
-    
-    superAgent.post('https://checkout.simplepay.ng/v1/payments/verify/')
+
+    superAgent.post('https://checkout.simplepay.ng/v2/payments/card/charge/')
         .set('Content-Type', 'application/json')
         .auth(privateKey, '')
-        .send({token: request.body.token})
+        .send({token: request.body.sp_token, amount: request.body.sp_amount, amount_currency: request.body.sp_currency})
         .end(function (err, res) {
             if (err || !res.ok) {
-                result.redirect('/failed/');
-            } else {  // even is http status code is 200 we still need to check transaction had issues or not
-                if (res.body.response_code === 20000){
+                if (request.body.sp_status === 'true') {
+                    // failed charge call, but card processed
                     result.redirect('/success/');
-                }else{
+                } else {
+                    result.redirect('/failed/');
+                }
+            } else {  // even is http status code is 200 we still need to check transaction had issues or not
+                if (res.body.response_code === 20000) {
+                    result.redirect('/success/');
+                } else {
                     result.redirect('/failed/');
                 }
             }
